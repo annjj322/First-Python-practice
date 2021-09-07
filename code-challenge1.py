bcdbd93b61e41e608eff495d1a6eae23ffa6028c
annjj322 <87896157+annjj322@users.noreply.github.com>
2021-09-07 15:46:08 +0900

    Create code-challenge1.py

---
+++ code-challenge1.py
@@ -0,0 +1,24 @@
+def plus(a,b):
+    a=int(a)
+    b=int(b)
+    return(a+b)
+
+def minus(a,b):
+    a=int(a)
+    b=int(b)
+    return(a-b)
+
+def mult(a,b):
+    a=int(a)
+    b=int(b)
+    return(a*b)
+
+def div(a,b):
+    a=int(a)
+    b=int(b)
+    return(a/b)
+
+print(mult("5",5))
+print(plus(5,4))
+print(div(9,3))
+print(minus(9,"6"))
