41cdd8b63adaf5e50c7d6633fe089ccb41021c50
annjj322 <87896157+annjj322@users.noreply.github.com>
2021-09-11 23:05:48 +0900

    6-1 if

--- Quiz/quize4.py
+++ Quiz/quize4.py
@@ -5,15 +5,11 @@
 from itertools import starmap
 from random import *
  
-lst = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+lst = list(range(1,21))
 shuffle(lst)
 chicken = list(sample(lst,1))
 cof_set = list(set(lst)-set(chicken))
 coffee = sample(cof_set,3)
-
-
-
-
 
 
 print(f"-- 당첨자 발표 -- \n 치킨 당첨자 : {chicken}\n 커피 당첨자 : {coffee}\n -- 축하합니다 --")
